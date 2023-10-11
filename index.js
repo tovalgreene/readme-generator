e49a366a735601799c901b15b74f0829eb12dca3
Tova Greene <tovalgreene@gmail.com>
2023-10-10 20:16:39 -0400

    edited starter code

--- index.js
+++ index.js
@@ -1,29 +1,22 @@
-const fs = require('fs');
+// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
-const generateMarkdown = require('./utils/generateMarkdown');
+const fs = require('fs');
+const generateMarkdown = require('./generateMarkdown');
 
-function promptUser() {
-    return inquirer.prompt([
-        {
-            type: 'input',
-            message: 'Enter your project title:',
-            name: 'title',
-        },
-        {
-            type: 'input',
-            message: 'Enter your project description:',
-            name: 'description',
-        },
-        {
-            type: 'input',
-            message: 'Enter installation instructions:',
-            name: 'installation',
-        },
-        {
-            type: 'input',
-            message: 'Enter usage info:',
-            name: 'usage',
-        },
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+      },
+];
 
-    ])
-}
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
