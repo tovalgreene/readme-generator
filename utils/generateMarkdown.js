c94cc30673de6ab1fc5683ecd936b2a25eddf713
Tova Greene <tovalgreene@gmail.com>
2023-10-10 21:58:47 -0400

    wrote generateMarkdown.js with the template

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,101 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) { 
+  if (license) {
+    switch (license) {
+      case 'MIT':
+        return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+      case 'Apache 2.0':
+        return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+      default:
+        return '';
+      }
+  }
+  return '';
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license) {
+    switch (license) {
+      case 'MIT':
+        return 'https://opensource.org/licenses/MIT';
+      case 'Apache 2.0':
+        return 'https://opensource.org/licenses/Apache-2.0';
+      default:
+        return ''; 
+    }
+  }
+  return ''; 
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license) {
+    return `## License
+
+This project is licensed under the [${license}](${renderLicenseLink(license)}) license.`;
+  }
+  return '';
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+
+  const licenseBadge = renderLicenseBadge(data.license);
+  const licenseSection = renderLicenseSection(data.license);
+
   return `# ${data.title}
+
+  ## Description
+
+    ${data.description}
+
+  ## Table of Contents 
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+  - [Badges] (#badges)
+  - [Features] (#features)
+  - [How to Contribute] (#howtocontribute)
+  - [Tests] (#tests)
+  
+  ${licenseSection}
+
+  ## Installation
+      ${data.installation}
+
+  
+  ## Usage
+  
+  ${data.usage}
+
+  ## Credits
+  ${data.credits}
+
+  
+  ## License
+  
+  ${data.description}
+
+  ## Badges
+  
+  ${licenseBadge}
+
+  ## Features
+  
+  ${data.features}
+
+  ## How to Contribute
+  
+    ${data.contribute}
+
+  ## Tests
+
+  ${data.tests}
 
 `;
 }
